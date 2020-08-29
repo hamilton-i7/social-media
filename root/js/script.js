31670bc8119745acc0e496ff599d3d19b87bd916
Juan Hamilton <hedwards507@gmail.com>
2020-08-28 21:53:07 -0500

    added button functionality & white theme

---
+++ root/js/script.js
@@ -0,0 +1,39 @@
+const body = document.querySelector(".js--body");
+const mainHeader = document.querySelector(".js--header");
+const mainTitle = document.querySelector(".js--mainTitle");
+const totalFollowers = document.querySelector(".js--totalFollowers");
+const mode = document.querySelector(".js--mode");
+const button = document.querySelector(".js--button");
+const buttonIndicator = document.querySelector(".js--buttonIndicator");
+const followersCards = document.querySelectorAll(".js--followersCard");
+const usernames = document.querySelectorAll(".js--username");
+const followersCounters = document.querySelectorAll(".js--followersCounter");
+const followersSpan = document.querySelectorAll(".js--followersSpan");
+const rate = document.querySelector(".js--rateNormal");
+const overviewHeader = document.querySelector(".js--overviewHeader");
+const overviewCards = document.querySelectorAll(".js--overviewCard");
+const overviewCardTitles = document.querySelectorAll(".js--overviewCardTitle");
+const overviewCardAmounts = document.querySelectorAll(".js--overviewCardAmount");
+
+const singleElements = [body, mainHeader, mainTitle, totalFollowers, mode, button, buttonIndicator, rate, overviewHeader];
+const repeatedElements = [followersCards, usernames, followersCounters, followersSpan, overviewCards, overviewCardTitles, overviewCardAmounts];
+
+
+button.addEventListener("click", toggleTheme);
+
+function toggleTheme() {
+    singleElements.forEach(function(el) {
+        activeToggle(el);
+    });
+
+    repeatedElements.forEach(function(list) {
+        list.forEach(function(el) {
+            activeToggle(el);
+        });
+    });
+}
+
+function activeToggle(el) {
+    el.classList.toggle("dark");
+    el.classList.toggle("light");
+}
